f5b92df73bbf323ad78c7c752d2a3546bf6267b7
victory-lydia <vickylydy@gmail.com>
2025-03-07 17:57:35 +0100

    bg fix

--- ship.py
+++ ship.py
@@ -29,7 +29,7 @@ class Ship(Sprite):
         # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left == 23:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Update rect object from self.x.
