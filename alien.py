d0665034f44ab81762a591d47af861394c9dcb5e
victory-lydia <vickylydy@gmail.com>
2025-03-07 18:00:54 +0100

    bg fix

--- alien.py
+++ alien.py
@@ -9,7 +9,8 @@ class Alien(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('')
+        self.image = pygame.image.load('images/alien.bmp')
+
         self.rect = self.image
         # Start each new alien near the top left of the screen.
         self.rect.x = self.width
