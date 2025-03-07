748ff64af8ca585d97b2dbee508badc894d556c6
victory-lydia <vickylydy@gmail.com>
2025-03-07 17:48:18 +0100

    bg fix

--- scoreboard.py
+++ scoreboard.py
@@ -55,8 +55,8 @@ class Scoreboard:
 
     def show_score(self):
         """Draw score to the screen."""
-        self.screen.fill(self.score_image, self.score_rect)
-        self.screen.draw(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
         self.ships.flip(self.screen)
 
