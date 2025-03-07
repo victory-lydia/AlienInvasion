d68ef9199245844b19704a5a297af35871ebcf32
victory-lydia <vickylydy@gmail.com>
2025-03-07 17:08:20 +0100

    bg fix

--- alien_invasion.py
+++ alien_invasion.py
@@ -54,7 +54,7 @@ class AlienInvasion:
         number_rows = available_space_y // (2 * alien_height)
 
         # Create the full fleet of aliens.
-        for row_number in range(0):
+        for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
 
@@ -65,7 +65,7 @@ class AlienInvasion:
         alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
-        alien.rect.y = alien.rect.height + alien.rect.height
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height + row_number
         self.aliens.add(alien)
 
     def _update_aliens(self):
