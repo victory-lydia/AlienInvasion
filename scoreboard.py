1e7a43a155d431fc82fce14cf03f1b268d661f5a
victory-lydia <vickylydy@gmail.com>
2025-03-07 17:46:02 +0100

    bg fix

--- scoreboard.py
+++ scoreboard.py
@@ -39,7 +39,7 @@ class Scoreboard:
         self.high_score_image = self.font.render(high_score_str, True,
                 self.text_color, self.settings.bg_color)
         # Center the high score at the top of the screen.
-        self.high_score_rect = self.high_score_image
+        self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
