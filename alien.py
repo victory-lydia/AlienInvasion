dea6abe80c236a95135518c557179fcfff1042e6
victory-lydia <vickylydy@gmail.com>
2025-03-07 18:04:37 +0100

    bg fix

--- alien.py
+++ alien.py
@@ -22,10 +22,10 @@ class Alien(Sprite):
     def check_edges(self):
         """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
-        if self.rect.right >= screen_rect.right and self.rect.left <= 0:
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
         
     def update(self):
         """Move the alien right or left."""
         self.x += (self.settings.alien_speed * self.settings.fleet_direction)
-        self.rect = self.x
+        self.rect.x = self.x
