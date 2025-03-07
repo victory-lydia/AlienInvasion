7f081c54407cfc66311bc2098644e59eff3d1899
victory-lydia <vickylydy@gmail.com>
2025-03-07 17:49:03 +0100

    bg fix

--- scoreboard.py
+++ scoreboard.py
@@ -58,7 +58,7 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
-        self.ships.flip(self.screen)
+        self.ships.draw(self.screen)
 
     def check_high_score(self):
         """Check to see if there's a new high score."""
