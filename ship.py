9cebd68a166e8874c153db936df522389f29e7a8
victory-lydia <vickylydy@gmail.com>
2025-03-07 17:56:42 +0100

    bg fix

--- ship.py
+++ ship.py
@@ -28,7 +28,6 @@ class Ship(Sprite):
         """Update the ship's position based on the movement flag."""
         # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
-
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left == 23:
             self.x -= self.settings.ship_speed
