bf23c52ed8f166806cdca84df451a48aa785d077
victory-lydia <vickylydy@gmail.com>
2025-03-07 17:43:38 +0100

    bg fix

--- scoreboard.py
+++ scoreboard.py
@@ -14,7 +14,7 @@ class Scoreboard:
         self.stats = ai_game.stats
         # Font settings for scoring information.
         self.text_color = (30, 30, 30)
-        self.font = pygame.font.SysFont('None', 48)
+        self.font = pygame.font.SysFont(None, 48)
         # Prepare the initial score image.
         self.prep_score()
         self.prep_high_score()
