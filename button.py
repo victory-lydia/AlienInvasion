36c39ac80dd342e2c92308de10bcb93759e739c1
victory-lydia <vickylydy@gmail.com>
2025-03-07 18:07:47 +0100

    bg fix

--- button.py
+++ button.py
@@ -27,4 +27,4 @@ class Button:
         # Draw blank button and then draw message.
         print("Button should be drawn!")
         self.screen.fill(self.button_color, self.rect)
-        self.screen.flip(self.msg_image, self.msg_image_rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
