8cf92afc13ac9a097c9fb2b508dbecd65a4f5ca1
victory-lydia <vickylydy@gmail.com>
2025-03-07 18:11:57 +0100

    bg fix

--- bullet.py
+++ bullet.py
@@ -20,7 +20,7 @@ class Bullet(Sprite):
     def update(self):
         """Move the bullet up the screen."""
         # Update the decimal position of the bullet.
-        self.y -= self
+        self.y -= self.settings.bullet_speed
         # Update the rect position.
         self.rect.y = self.y
 
